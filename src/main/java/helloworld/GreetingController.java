4532545611e9976b18ec5845bffaf241e6394e87
iedexmain3 <userwpjhb4@some.mail>
2017-12-13 21:24:05 +0000

    commitchk_remoteWed Dec 13 21:24:04 UTC 2017

--- src/main/java/helloworld/GreetingController.java
+++ src/main/java/helloworld/GreetingController.java
@@ -1,5 +1,5 @@
 package helloworld;
-//1513111248576
+//1513200111754
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,4 +1,4 @@
-<!1513111294908>
+<!1513200157869>
 <%
    response.sendRedirect("spring/hello");
 %>
